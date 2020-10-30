bc2c3df0dff2bfef0a3084e2a091085835198144
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 15:48:06 +0530

    [Ramya] Add . Added UC_3

--- Day-07-EmployeeWageComputation/Program.cs
+++ Day-07-EmployeeWageComputation/Program.cs
@@ -17,6 +17,8 @@ namespace Day_07_EmployeeWageComputation
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
+            //Checks if employee works full time
+            //or part time
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;

c8a5dcc07094c5882faa79abd7e686a8623dc824
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 17:14:37 +0530

    [Ramya] Add . Added UC_9

---
+++ Day-07-EmployeeWageComputation/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class Class1
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dmart = new EmpWageBuilderObject("Dmart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dmart.computeEmpWage();
+            Console.WriteLine(dmart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
+    }
+}
--- Day-07-EmployeeWageComputation/Program.cs
+++ Day-07-EmployeeWageComputation/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
-
-namespace Day_07_EmployeeWageComputation
+namespace EmployeeWages
 {
-    class Program
+    public class EmpWageBuilderObject
     {
-        static void Main(string[] args)
+        //constants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmpWageBuilderObject"/> class.
+        /// </summary>
+        /// <param name="company">The company.</param>
+        /// <param name="empRatePerHour">The emp rate per hour.</param>
+        /// <param name="numOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHoursPerMonth">The maximum hours per month.</param>
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation!");
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
+        public void computeEmpWage()
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            ///While loop is used to check maximum working hours and number of working days.
+            ///if it is true then it executes
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp hrs " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage For Company:" + company + " is :" + totalEmpWage);
+        }
+        /// <summary>
+        /// To the string.
+        /// </summary>
+        /// <returns>Total employee wage of each company</returns>
+        public string toString()
+        {
+            return "Total Emp Wage for company: " + this.company + " is : " + this.totalEmpWage;
         }
     }
 }

3fe5d769ada3a8bfd2159e03175468fd7f2aea37
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 15:47:02 +0530

    [Ramya] Add . Added UC_3

--- Day-07-EmployeeWageComputation/Program.cs
+++ Day-07-EmployeeWageComputation/Program.cs
@@ -7,6 +7,30 @@ namespace Day_07_EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
+            //constants
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int IS_PART_TIME = 1;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }

ed20b917c7e45a741d357f6972a9b05aae3e3679
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 17:32:27 +0530

    [Ramya] Add . Added UC_10

---
+++ Day-07-EmployeeWageComputation/CompanyEmpWage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+
+namespace Day_07_EmployeeWageComputation
+{
+    public class CompanyEmpWage
+    {
+        //variables
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompanyEmpWage"/> class.
+        /// </summary>
+        /// <param name="company">The company.</param>
+        /// <param name="empRatePerHour">The emp rate per hour.</param>
+        /// <param name="numOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHoursPerMonth">The maximum hours per month.</param>
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            //this is used to refer the current instances
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        
+        //this method returns the string itself
+        public string toString()
+        {
+            return "Total Emp Wage for company: " + this.company + " is: " + this.totalEmpWage;
+        }
+    }
+}

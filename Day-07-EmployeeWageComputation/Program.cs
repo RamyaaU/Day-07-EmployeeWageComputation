efaf2e3e06c3e5afaff6e13e51914f5682102557
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 15:40:35 +0530

    [Ramya] Add . Added UC_1

--- Day-07-EmployeeWageComputation/Program.cs
+++ Day-07-EmployeeWageComputation/Program.cs
@@ -7,6 +7,22 @@ namespace Day_07_EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
+            //variable
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            //checks if employee 
+            //is present
+            //or absent
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }

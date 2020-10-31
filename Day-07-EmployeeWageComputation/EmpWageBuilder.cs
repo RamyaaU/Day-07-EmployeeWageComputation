a54838ff7747c7e831bac413fc47b319255b91e9
Ramya.U <ramyayadav2016@gmail.com>
2020-10-31 17:24:16 +0530

    [Ramya] Add . Added UC_12

---
+++ Day-07-EmployeeWageComputation/EmpWageBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Day_07_EmployeeWageComputation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    public class EmpWageBuilder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation");
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
--- Day-07-EmployeeWageComputation/Program.cs
+++ Day-07-EmployeeWageComputation/Program.cs
@@ -4,9 +4,7 @@ namespace Day_07_EmployeeWageComputation
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation!");
-        }
+
     }
 }
+        

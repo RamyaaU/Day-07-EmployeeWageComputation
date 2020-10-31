b7a5cca8d21869dbcf696e1fb42b8f7abfe600b7
Ramya.U <ramyayadav2016@gmail.com>
2020-10-31 17:37:45 +0530

    [Ramya] Add . Added UC_13

---
+++ Day-07-EmployeeWageComputation/EmpWageBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_07_EmployeeWageComputation
+{
+    public class EmpWageBuilder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation");
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
---
+++ Day-07-EmployeeWageComputation/EmpWageBuilderArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_07_EmployeeWageComputation
+{
+    public class EmpWageBuilderArray : IEmployeeWage
+    {
+        //constants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        private int numOfCompany = 0;
+
+        //creates a list of companyempwage
+        List<CompanyEmpWage> companyEmpWageArray = new List<CompanyEmpWage>();
+
+        /// <summary>
+        /// Adds the company emp wage.
+        /// </summary>
+        /// <param name="company">The company.</param>
+        /// <param name="empRatePerHour">The emp rate per hour.</param>
+        /// <param name="numOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHoursPerMonth">The maximum hours per month.</param>
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            companyEmpWageArray.Add(new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth));
+            numOfCompany++;
+        }
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
+        public void computeEmpWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
+            }
+        }
+
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
+        /// <param name="companyEmpWage">The company emp wage.</param>
+        /// <returns></returns>
+        public int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp hrs " + empHrs + "  Daily Wage:" + (empHrs * companyEmpWage.empRatePerHour));
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+    }
+}
